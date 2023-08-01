239211e06b5a34cc4b0206425c2db4eb410493de
Dmitrii Rudik <dima.rudik@gmail.com>
2023-08-01 15:59:05 +0500

    Postman

---
+++ src/main/java/ru/otus/docker/model/InternetAddress.java
@@ -0,0 +1,16 @@
+package ru.otus.docker.model;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+
+public record InternetAddress (InetAddress inetAddress, String hostAddress) {
+    @Override
+    public InetAddress inetAddress() {
+        return inetAddress;
+    }
+
+    @Override
+    public String hostAddress() {
+        return hostAddress;
+    }
+}
